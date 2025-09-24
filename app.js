c5fe03d6562ecceb5a5466140d2568a0052899c4
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-09-23 23:25:12 -0300

    criada função `adicionarAmigo`

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaAmigos = [];
+
+function adicionarAmigo() {
+  let campoDigiteNome = document.querySelector("input").value;
+
+  if (campoDigiteNome.length != 0) {
+    listaAmigos.push(campoDigiteNome);
+    document.querySelector("input").value = "";
+    document.querySelector("input").focus();
+  } else {
+    alert("Você precisa digitar um nome!");
+    document.querySelector("input").focus();
+  }
+}
+
+function sortearAmigo() {}
