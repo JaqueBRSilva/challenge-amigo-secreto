fa6578695eea849b40ee59389ea7cc32d1f0fb79
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-09-24 16:01:59 -0300

    refatorada função p/ recusar nome já inserido na lista

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 // O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
 // Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let listaAmigos = [];
+let listaAmigosAdd = [];
+let listaAmigos = document.querySelector("#listaAmigos");
+let resultado = document.querySelector("#resultado");
 
 function adicionarAmigo() {
   let campoDigiteNome = document.querySelector("input").value;
 
-  if (campoDigiteNome.length != 0) {
-    listaAmigos.push(campoDigiteNome);
+  if (campoDigiteNome.length != 0 && !listaAmigosAdd.includes(campoDigiteNome)) {
+    listaAmigosAdd.push(campoDigiteNome.toUpperCase());
+    listaAmigos.innerHTML += `<li>${campoDigiteNome}</li>`;
     document.querySelector("input").value = "";
-    document.querySelector("input").focus();
+
   } else {
-    alert("Você precisa digitar um nome!");
-    document.querySelector("input").focus();
+    if (campoDigiteNome.length == 0) {
+      alert("Por favor, insira um nome");
+    } else if (listaAmigosAdd.includes(campoDigiteNome)) {
+      alert("Este nome já foi adicionado anteriormente");
+    }
   }
+
+  document.querySelector("input").focus();
 }
 
 function sortearAmigo() {}
